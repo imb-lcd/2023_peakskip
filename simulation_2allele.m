019a4cff61ee2dab76b159b3bfc15ba20b9765ca
imb-lcd <imb.lcd@gmail.com>
2023-10-02 14:58:18 +0800

    Summary
    Source codes for simulating and detecting peak skips

---
+++ simulation_2allele.m
@@ -0,0 +1,223 @@
+%% Initialize
+clear; close all; clc
+
+% Add path
+addpath(genpath("../model_data"));
+
+%% Stochiometry
+S = readtable('/stoichiometry.txt');
+S = table2array(S(1:11,2:23));
+
+%% Parameters
+L0 = 0.6; % Time scale
+c0 = 1.7; % Level scale
+
+% p53
+alpha_p = 0.2/L0/c0;  % units: [M/h]
+beta_p = 123/L0/c0; % units: [1/M*1/h]
+
+% MDM2 mRNA
+alpha_m = 1/L0/c0; % units: [M/h]
+beta_m = 1/L0; % units: [1/h]
+
+% Factor for KD treatments
+gamma1 = 1;
+gamma2 = 1;
+
+% Mdm2 protein
+alpha_M = 1/L0; % units: [1/h]
+beta_M = 1/L0; % units: [1/h]
+
+% q mRNA
+alpha_q = 0.38/L0/c0; % units: [1/M*1/h]
+beta_q = 1/L0; % units: [1/h]
+
+% Q protein
+alpha_Q = 1/L0; % units: [1/h]
+beta_Q = 1/L0; % units: [1/h]
+
+% Transcription switch
+alpha_T = (3.5*10^1.35)/L0/(c0^2); % units: [1/M^2*1/h]
+beta_T = 1.2/L0; % units: [1/h]
+
+% Complex
+phi = (10^0.79)/L0/c0; % units: [1/M*1/h]
+psi = (10^0.3)/L0; % units: [1/h]
+
+% Scale
+sig = 6.02*0.2*10^2;
+alpha_p = alpha_p*sig; beta_p = beta_p/sig; alpha_m = alpha_m*sig;...
+    alpha_T = alpha_T/(sig^2); phi = phi/sig; alpha_q = alpha_q/sig;
+
+% Collect parameters
+SimPars = [alpha_p,beta_p,...
+    alpha_m,beta_m,...
+    gamma1,gamma2,...
+    alpha_M,beta_M,...
+    alpha_q,beta_q,...
+    alpha_Q,beta_Q,...
+    alpha_T,beta_T,...
+    phi,psi];
+
+%% Simulation
+
+% Generate RNG for Gillespie algorithm
+defaultStream = RandStream('mlfg6331_64');
+RandStream.setGlobalStream(defaultStream)
+defaultStream.Substream = 1;
+
+% Initial condition =======================================================
+X = [0, 0,  0,  0,  0,  0,  0,  0,  0,  0,	0];
+%   [p, T1, m1, M1, T2, m2, M2, C1, C2, q,  Q];
+
+% Simulation ==============================================================
+ts = 0.5; click = 0; Tmax = 400; RT = 0;
+SimVal = [];
+
+while RT < Tmax
+    
+    % DNA damage-dependent initial Mdm2 suppresion ------------------------
+    if RT < 4
+        lambda_M = 10;
+        lambda_C = 0.1;
+    else
+        lambda_M = 1;
+        lambda_C = 1;
+    end
+    
+    % Reactants -----------------------------------------------------------
+    p = X(1); % p53
+    T1 = X(2); T2 = X(5); % TX state
+    m1 = X(3); m2 = X(6); % Mdm2 mRNA
+    M1 = X(4); M2 = X(7); % Mdm2 protein
+    C1 = X(8); C2 = X(9); % Complex
+    q = X(10); Q = X(11); % q mRNA and Q protein
+    
+    % Rates ---------------------------------------------------------------
+    R(1) = alpha_p;  % Unbound p53 production
+    R(2) = alpha_T*(1-T1)*p^2;  R(8) = alpha_T*(1-T2)*p^2; % p53-depedent TX ON
+    R(3) = beta_T*T1;           R(9) = beta_T*T2;    % TX OFF
+    R(4) = alpha_m*T1;          R(10) = alpha_m*T2;  % MDM2 mRNA production
+    R(5) = gamma1*beta_m*m1;    R(11) = gamma2*beta_m*m2;   % MDM2 mRNA degradation
+    R(6) = alpha_M*m1;          R(12) = alpha_M*m2;  % Mdm2 protein production
+    R(7) = beta_M*M1*lambda_M;  R(13) = beta_M*M2*lambda_M; % Mdm2 protein degradation
+    R(14) = phi*p*M1*lambda_C;  R(16) = phi*p*M2*lambda_C;  % Complex formation
+    R(15) = psi*C1;             R(17) = psi*C2;   % Complex dissociation
+    R(18) = alpha_q*p^2;    % q mRNA production
+    R(19) = beta_q*q;       % q mRNA degradation
+    R(20) = alpha_Q*q;      % Q protein production
+    R(21) = beta_Q*Q;       % Q protein degradation
+    R(22) = beta_p*p*Q;     % Q-dependent p53 degradation
+    
+    % Gillespie algorithm -------------------------------------------------
+    Rtot = sum(R);  % Total propensity
+    r1 = rand();    % To choose timing of next reaction
+    RT = RT - log(r1)/Rtot; % Timing of next reaction
+    r2 = rand();    % To choose the next reaction
+    idx = find(cumsum(R)/Rtot >= r2,1); % Index of next reaction
+    
+    X = X + S(:,idx)';  % Update reactants level after reaction
+    X(X<0) = 0; % Prevent negatives
+    
+    % Output --------------------------------------------------------------
+    if (RT > click*ts) % Record outputs at defined intervals only
+        sim = zeros(1,8);
+        sim(1) = click*ts;          % Time vector
+        sim(2) = X(1)+X(8)+X(9);    % Total p53
+        sim(3) = X(3);              % m1
+        sim(4) = X(4)+X(8);         % Total M1
+        sim(5) = X(6);              % m2
+        sim(6) = X(7)+X(9);         % Total M2
+        sim(7) = X(10);             % q
+        sim(8) = X(11);             % Q
+        SimVal = [SimVal;sim];
+        click = click + 1;
+    end
+    
+end
+
+%% Signal processing
+t = SimVal(:,1);    % Time vector
+pTot = movmean(SimVal(:,2),3);  % Total p53
+m1 = SimVal(:,3);
+M1Tot = movmean(SimVal(:,4),3); % Total M1
+m2 = SimVal(:,5);
+M2Tot = movmean(SimVal(:,6),3); % Total M2
+q = SimVal(:,7);
+Q = SimVal(:,8);
+
+pTot = detrend(pTot,3);
+M1Tot = detrend(M1Tot,3);
+M2Tot = detrend(M2Tot,3);
+
+%% Plot output
+close all
+c=colororder;
+fg=figure('Position',[2 32 958 964]);
+tiledlayout(5,1,'Padding','compact','TileSpacing','compact')
+
+% p53
+nexttile(1)
+plot(t,pTot,'Color',c(1,:),'LineWidth',2)
+ylabel('p53')
+ylim([-20 40])
+
+% Mdm2 protein allele #1
+nexttile(2)
+plot(t,M1Tot,'Color',c(2,:),'LineWidth',2)
+ylabel('Mdm2 allele 1')
+ylim([-40 80])
+
+% Mdm2 protein allele #2
+nexttile(3)
+plot(t,M2Tot,'Color',c(3,:),'LineWidth',2)
+ylabel('Mdm2 allele 2')
+ylim([-40 80])
+
+% q mRNA
+nexttile(4)
+plot(t,q,'Color','k','LineWidth',2)
+ylabel('q mRNA')
+
+% Q protein
+nexttile(5)
+plot(t,Q,'Color','k','LineWidth',2)
+ylabel('Q protein')
+
+%% Get statistics
+
+% Peaks
+[~,plocs]=findpeaks(pTot,'MinPeakProminence',2,'MinPeakDistance',3);
+[~,M1locs]=findpeaks(M1Tot,'MinPeakProminence',1.3,'MinPeakDistance',3);
+[~,M2locs]=findpeaks(M2Tot,'MinPeakProminence',1.3,'MinPeakDistance',3);
+
+% Valleys
+ptrlocs=arrayfun(@(i) find(pTot(plocs(i):plocs(i+1))...
+    ==min(pTot(plocs(i):plocs(i+1))))+plocs(i)-1,1:length(plocs)-1);
+M1trlocs=arrayfun(@(i) find(M1Tot(M1locs(i):M1locs(i+1))...
+    ==min(M1Tot(M1locs(i):M1locs(i+1))))+M1locs(i)-1,1:length(M1locs)-1);
+M2trlocs=arrayfun(@(i) find(M2Tot(M2locs(i):M2locs(i+1))...
+    ==min(M2Tot(M2locs(i):M2locs(i+1))))+M2locs(i)-1,1:length(M2locs)-1);
+
+% Mark peaks
+nexttile(2)
+hold on
+ylimit=get(gca,'YLim');
+plot(t(plocs),0.9*ylimit(2)*ones(size(plocs)),'|','LineStyle','none'...
+    ,'MarkerFaceColor',c(1,:),'MarkerEdgeColor',c(1,:),'MarkerSize',8 ...
+    ,'LineWidth',1.5);
+plot(t(M1locs),0.9*ylimit(2)*ones(size(M1locs)),'o','LineStyle','none'...
+    ,'MarkerFaceColor',c(2,:),'MarkerEdgeColor',c(2,:),'MarkerSize',4);
+
+nexttile(3)
+hold on
+ylimit=get(gca,'YLim');
+plot(t(plocs),0.9*ylimit(2)*ones(size(plocs)),'|','LineStyle','none'...
+    ,'MarkerFaceColor',c(1,:),'MarkerEdgeColor',c(1,:),'MarkerSize',8 ...
+    ,'LineWidth',1.5);
+plot(t(M2locs),0.9*ylimit(2)*ones(size(M2locs)),'o','LineStyle','none'...
+    ,'MarkerFaceColor',c(3,:),'MarkerEdgeColor',c(3,:),'MarkerSize',4);
+
+%% Skip detection
+skip_detection_allele1_single
+skip_detection_allele2_single
---
+++ skip_detection_allele2_single.m
@@ -0,0 +1,184 @@
+%% Dynamic time warping
+dista = zeros(length(pTot));
+for i = 1:length(pTot)
+    for j = 1:length(pTot)
+        dista(i,j) = (pTot(i)-M2Tot(j))^2;
+    end
+end
+
+% Calculate Accumulative dists (DTW)
+AccCos = zeros(length(pTot));
+for i = 2:length(pTot)
+    AccCos(1,i) = dista(1,i) + AccCos(1, i-1);
+end
+for i = 2:length(pTot)
+    AccCos(i,1) = dista(i,1) + AccCos(i-1, 1);
+end
+for i = 2:length(pTot)
+    for j = 2:length(pTot)
+        AccCos(i,j) = min([AccCos(i-1,j-1), AccCos(i-1,j), AccCos(i,j-1)]) + dista(i,j);
+        if (i>=j)
+            AccCos(i,j) = AccCos(i,j)*100;
+        end
+        if (j>i+3)
+            AccCos(i,j) = AccCos(i,j)*(j-i);
+        end
+    end
+end
+
+% Find path (DTW)
+path = [];
+i = length(pTot)-1;
+j = length(M2Tot)-1;
+while i>1 && j>1
+    if i==1
+        j = j - 1;
+    elseif j==1
+        i = i - 1;
+    else
+        if AccCos(i-1,j) == min([AccCos(i-1,j-1), AccCos(i-1,j), AccCos(i,j-1)])
+            i = i - 1;
+        elseif AccCos(i,j-1) == min([AccCos(i-1,j-1), AccCos(i-1,j), AccCos(i,j-1)])
+            j = j-1;
+        else
+            i = i - 1;
+            j= j- 1;
+        end
+    end
+    path = [path; j i];
+end
+path = [path; 1 1];
+path = flip(path);
+warp_p53 = pTot(path(:,2));
+warp_mdm2 = M2Tot(path(:,1));
+
+%% Plot warped signals
+
+figure('Position',[959   694   958   300])
+nexttile(1)
+plot(warp_p53,'LineWidth',1.5);
+xlim([1 max([length(warp_p53),length(warp_mdm2)])]);
+
+hold on
+plot(warp_mdm2,'LineWidth',1.5);
+ylabel('Level')
+
+%% Calculate velocity
+vp=[NaN;diff(warp_p53)];
+vm=[NaN;diff(warp_mdm2)];
+
+%% Skip detection
+
+skip_p53=NaN(size(M2Tot));
+skip_mdm2=NaN(size(M2Tot));
+skip_start=NaN(size(M2Tot));
+skip_p53_dtw=NaN(size(warp_mdm2));
+skip_mdm2_dtw=NaN(size(warp_mdm2));
+skip_start_dtw=NaN(size(warp_mdm2));
+skip_duration=[];
+skip_duration_dtw=[];
+fft_range=[];
+riseloc=[];
+skip_per_p53=[];
+skip_ratio=[];
+
+for i=2:length(plocs)-1
+    
+    % Find Mdm2 peak between 2 p53 peaks
+    idx=find(and(M2locs>=plocs(i)...
+        ,M2locs<=plocs(i+1)));
+    
+    % If no Mdm2 peak between 2 p53 peaks
+    if isempty(idx)
+        
+        x2=plocs(i);    % p53 peaks of interests
+        
+        x3=plocs(i-1);  % Previous p53 peak
+        x4=plocs(i+1);  % Next p53 peak
+        
+        x5=ptrlocs(and(ptrlocs>x3,ptrlocs<x2)); % Previous p53 valley
+        x6=ptrlocs(and(ptrlocs>x2,ptrlocs<x4)); % Next p53 valley
+
+        % Convert to warped coordinates
+        xx2=find(path(:,2)==x2,1,'first');
+
+        xx3=find(path(:,2)==x3,1,'last');
+
+        xx4=find(path(:,2)==x4,1,'first');
+        if isempty(xx4)
+            continue
+        end
+        
+        xx5=find(path(:,2)==x5,1,'last');
+
+        xx6=find(path(:,2)==x6,1,'first');
+        
+        % Filter with relative velocity
+        vp1=vp(xx5:xx2);
+        vp_pos=sum(vp1(vp1>0));
+        vM2=vm(xx5:xx2);
+        vm_pos=sum(vM2(vM2>0));
+        riv=vm_pos/vp_pos;
+        
+        if riv>10^(-1.2)
+            continue
+        end
+        
+        % FFT filter
+        L=x4-x3+1;
+        Fs=2;
+        X=pTot(x3:x4);
+        X=detrend(X,2);
+        Y=fft(X);
+        P2 = abs(Y/L);
+        if mod(L,2)==0
+            P1 = P2(1:L/2+1);
+            P1(2:end-1) = 2*P1(2:end-1);
+            f = Fs*(0:(L/2))/L;
+        else
+            P1 = P2(1:(L-1)/2+1);
+            P1(2:end-1) = 2*P1(2:end-1);
+            f = Fs*(0:(L-1)/2)/L;
+        end
+        
+        [maxpw,idx]=max(P1(f>0));
+        maxfreq=f(P1==maxpw);
+        main_per_p53=1/maxfreq;
+        
+        if main_per_p53>=7.375|| main_per_p53<2.5
+            continue
+        end
+        
+        skip_per_p53=vertcat(skip_per_p53,main_per_p53);
+        skip_ratio=vertcat(skip_ratio,riv);
+        fft_range=vertcat(fft_range,[path(x3,2), path(x4,2)]);
+        
+        skip_p53(path(xx5,2):path(xx6,2))=pTot(path(xx5,2):path(xx6,2));
+        skip_mdm2((path(xx5,1)):(path(xx6,1)))=M2Tot((path(xx5,1)):(path(xx6,1)));
+        
+        skip_p53_dtw(xx5:xx6)=warp_p53(xx5:xx6);
+        skip_mdm2_dtw(xx5:xx6)=warp_mdm2(xx5:xx6);
+
+        skip_start(path(xx2,2))=50;
+
+        detection_point{2}=vertcat(detection_point{2},plocs(i));
+        
+    end
+    
+end
+
+%% Add markers to figure
+figure(fg)
+nexttile(1);
+hold on
+plot(t,skip_p53,'b','LineWidth',2.5);
+plot(t,skip_start-20,'LineStyle','none','Marker','^' ...
+    ,'MarkerFaceColor',c(3,:),'MarkerEdgeColor','k'...
+    ,'MarkerSize',6);
+
+nexttile(3);
+hold on
+plot(t,skip_mdm2,'r','LineWidth',2.5);
+plot(t,skip_start,'LineStyle','none','Marker','^' ...
+    ,'MarkerFaceColor',c(3,:),'MarkerEdgeColor','k'...
+    ,'MarkerSize',6);
